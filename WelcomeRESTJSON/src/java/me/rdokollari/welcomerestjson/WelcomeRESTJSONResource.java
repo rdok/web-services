12e874c12622f3686e9c1001c1720c7df516135a
Rizart Dokollari <r.dokollari@acg.edu>
2014-02-27 09:09:20 +0200

    Adds MIT license to class code.

--- WelcomeRESTJSON/src/java/me/rdokollari/welcomerestjson/WelcomeRESTJSONResource.java
+++ WelcomeRESTJSON/src/java/me/rdokollari/welcomerestjson/WelcomeRESTJSONResource.java
@@ -1,8 +1,27 @@
-/**
- * REST Web Service that returns a welcome message as JSON.
- * @author Rizart Dokollari <RizartDokollari@rdokollari.me>
- * @since Feb 27, 2014
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Rizart Dokollari <RizartDokollari@rdokollari.me>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
+
 
 package me.rdokollari.welcomerestjson;
 
@@ -13,7 +32,11 @@ import javax.ws.rs.GET;       // annotation to indicate method usees HTTP GET
 import javax.ws.rs.PathParam; // annotation to get parameters from URI
 import javax.ws.rs.Produces;  // annotation to specify type of data
 
-
+/**
+ * REST Web Service that returns a welcome message as JSON.
+ * @author Rizart Dokollari <RizartDokollari@rdokollari.me>
+ * @since Feb 27, 2014
+ */
 @Path("welcome")  // path used to access the resource
 public class WelcomeRESTJSONResource {
 
